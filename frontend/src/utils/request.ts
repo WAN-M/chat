6fd087e2f67a9eef74dfcc5d36b2d37e6744859b
chinobody <2216022759@qq.com>
2024-12-15 15:34:22 +0800

    MOD: modify some structure

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -13,12 +13,12 @@ request.interceptors.response.use(
     return res.data
   },
   ({ res }) => {
-    if (res.data.code !== 200) {
-      ElMessage.warning({ message: res.data.message })
+    if (res.code !== 200) {
+      ElMessage.warning({ message: res.message })
     }
     // FIX 似乎没起作用，后面再调试
     // token存储在cookie中，如果之前登录过，想重新登录，此时发送登录请求同样会携带token，不符合预期：1. 前端不携带token 2. 后端不验证登录接口的token
-    if (res.data.code === 401) {
+    if (res.code === 401) {
       router.push('/login')
     }
     return Promise.reject(res.data)
